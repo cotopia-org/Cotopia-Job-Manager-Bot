b8f9222bfc8d81af75121beaf5fb71151f332615
Ali Kharrati <ali.kharrati@gmail.com>
2023-12-19 08:16:49 +0330

    ADD bot_auth

---
+++ bot_auth.py
@@ -0,0 +1,17 @@
+import datetime
+from os import getenv
+
+import pytz
+from dotenv import load_dotenv
+from jose import jwt
+
+
+def create_token(d: dict):
+    load_dotenv()
+    token = None
+    now = datetime.datetime.now(tz=pytz.utc)
+    expires_at = now + datetime.timedelta(0, 300)  # 5 mins later
+    d["expires_at"] = expires_at.strftime("%Y-%m-%dT%H:%M:%S%z")
+    d["is_genuine"] = getenv("PEPPER")
+    token = jwt.encode(d, getenv("SALT"), algorithm="HS256")
+    return token
