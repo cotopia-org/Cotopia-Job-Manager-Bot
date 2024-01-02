da568cc49455021a341b02099c9149be148a4586
Ali Kharrati <ali.kharrati@gmail.com>
2024-01-02 10:19:03 +0330

    ADD old brief feature

---
+++ briefing/briefing.py
@@ -0,0 +1,128 @@
+# from discord import VoiceState, Member
+import datetime
+import time
+import psycopg2
+
+
+# ✅
+def should_record_brief(doer: str, driver: str):
+    last = get_last_brief_ts(doer, driver)
+    if (last == -1):
+        return True
+    now = today()
+    print(f"last recorded brief for {doer}@{driver} was from {last}")
+    if (last == now):
+        return False
+    else:
+        return True
+
+# ✅
+def get_last_brief(doer: str, driver: str):
+    conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
+                        password="Tp\ZS?gfLr|]'a", port=5432)
+    cur = conn.cursor()
+    cur.execute("""
+                   SELECT content FROM brief
+                   WHERE doer = %s AND driver = %s
+                   ORDER BY ts DESC;
+                   """, (doer, driver))
+    
+    result = cur.fetchone()
+
+    if (result is None):
+        print("no brief found!")
+        conn.commit()
+        cur.close()
+        conn.close()
+        return "N/A"
+    else:
+        conn.commit()
+        cur.close()
+        conn.close()
+        return result[0]
+
+# ✅
+def get_last_brief_epoch(doer: str, driver: str):
+    conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
+                        password="Tp\ZS?gfLr|]'a", port=5432)
+    cur = conn.cursor()
+    cur.execute("""
+                   SELECT epoch FROM brief
+                   WHERE doer = %s AND driver = %s
+                   ORDER BY epoch DESC;
+                   """, (doer, driver))
+    
+    result = cur.fetchone()
+
+    if (result is None):
+        print("no brief found!")
+        conn.commit()
+        cur.close()
+        conn.close()
+        return -1
+    else:
+        conn.commit()
+        cur.close()
+        conn.close()
+        return result[0]
+
+# returns epoch of NOW: int
+def rightnow():
+    epoch = int(time.time())
+    return epoch
+
+# ✅
+def write_to_db(brief: str, doer: str, driver: str):
+    conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
+                        password="Tp\ZS?gfLr|]'a", port=5432)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO brief (driver, epoch, doer, content) VALUES (%s, %s, %s, %s);",
+                 (driver, rightnow(), doer, brief))
+    print("trying to write a brief to db!")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+# ✅
+def create_table():
+    conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
+                        password="Tp\ZS?gfLr|]'a", port=5432)
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS brief(
+            id SERIAL NOT NULL PRIMARY KEY,
+            driver VARCHAR(255) null,
+            ts TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+            epoch INTEGER null,
+            doer VARCHAR(255) null,
+            content TEXT null);""")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def today():
+    the_string = datetime.datetime.today().strftime('%Y-%m-%d')
+    return the_string
+
+def get_last_brief_ts(doer: str, driver: str):
+    conn = psycopg2.connect(host="localhost", dbname="postgres", user="postgres",
+                        password="Tp\ZS?gfLr|]'a", port=5432)
+    cur = conn.cursor()
+    cur.execute("""
+                   SELECT ts FROM brief
+                   WHERE doer = %s AND driver = %s
+                   ORDER BY epoch DESC;
+                   """, (doer, driver))
+    
+    result = cur.fetchone()
+
+    if (result is None):
+        print("no brief found!")
+        conn.commit()
+        cur.close()
+        conn.close()
+        return -1
+    else:
+        conn.commit()
+        cur.close()
+        conn.close()
+        return result[0].strftime('%Y-%m-%d')
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
+import asyncio
+import time
+
 import discord
 import requests
 from discord.ext import commands
+from persiantools.jdatetime import JalaliDate
 
 import settings
 from bot_auth import create_token
+from briefing import briefing
 from modals.submit import JobSubmitModal
 
 logger = settings.logging.getLogger("bot")
+
+last_brief_ask = {}
+
+
+# returns epoch of NOW: int
+def rightnow():
+    epoch = int(time.time())
+    return epoch
 
 
 def run():
@@ -22,6 +35,127 @@ def run():
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
         await bot.tree.sync()
+
+    @bot.event
+    async def on_message(message):
+        if message.author == bot.user:
+            return
+        # RECORDING BRIEF
+        try:
+            replied_to = await message.channel.fetch_message(
+                message.reference.message_id
+            )
+            if replied_to.author == bot.user:
+                if "Reply to this message to submit a brief." in replied_to.content:
+                    if message.author in replied_to.mentions:
+                        briefing.write_to_db(
+                            brief=message.content,
+                            doer=str(message.author),
+                            driver=str(message.guild.id),
+                        )
+                        em = discord.Embed(
+                            title="#brief",
+                            description=message.content,
+                            color=discord.Color.blue(),
+                        )
+                        em.set_author(name=str(JalaliDate.today()))
+                        channel = message.guild.system_channel
+                        if channel is None:
+                            channel = message.guild.text_channels[0]
+                        webhook = await channel.create_webhook(name=message.author.name)
+                        if message.author.nick is None:
+                            the_name = message.author.name
+                        else:
+                            the_name = message.author.nick
+                        await webhook.send(
+                            embed=em,
+                            username=the_name,
+                            avatar_url=message.author.avatar,
+                        )
+                        webhooks = await channel.webhooks()
+                        for w in webhooks:
+                            await w.delete()
+                        await replied_to.delete()
+                        await message.delete()
+                        try:
+                            (task,) = [
+                                task
+                                for task in asyncio.all_tasks()
+                                if task.get_name()
+                                == f"ask for brief {str(message.author)}@{message.guild.id}"
+                            ]
+                            task.cancel()
+                        except:  # noqa: E722
+                            print("Asking for brief was not canceled! Don't panic tho.")
+
+        except:  # noqa: E722
+            print("the message is not relevant!")
+
+    @bot.event
+    async def on_voice_state_update(member, before, after):
+        # Ignoring Bots
+        if member.bot:
+            return
+
+        guild = member.guild
+
+        # func that asks for brief after a while
+        task2 = None
+
+        async def ask_for_brief():
+            await asyncio.sleep(8)  # 8 seconds
+            try:
+                await guild.system_channel.send(
+                    "Welcome "
+                    + member.mention
+                    + "!\nWhat are you going to do today?\nReply to this message to submit a brief."
+                )
+            except:  # noqa: E722
+                await guild.text_channels[0].send(
+                    "Welcome "
+                    + member.mention
+                    + "!\nWhat are you going to do today?\nReply to this message to submit a brief."
+                )
+
+        # When user leaves voice channel
+        if after.channel is None:
+            # cancelling asking for brief
+            try:
+                (task,) = [
+                    task
+                    for task in asyncio.all_tasks()
+                    if task.get_name() == f"ask for brief {str(member)}@{guild.id}"
+                ]
+                task.cancel()
+            except:  # noqa: E722
+                print("Asking for brief was not canceled! Don't panic tho.")
+
+        # ASKING FOR BRIEF
+        global last_brief_ask
+
+        def get_previous_ask(doer: str):
+            try:
+                return rightnow() - last_brief_ask[doer + "@" + str(guild.id)]
+            except:  # noqa: E722
+                return 1000000000
+
+        def just_asked(doer: str):
+            if get_previous_ask(doer) < 8:  # 8 seconds
+                return True
+            else:
+                return False
+
+        if before.channel is None:
+            if briefing.should_record_brief(driver=str(guild.id), doer=str(member)):
+                if just_asked(str(member)) is False:
+                    # Ask 8 seconds later
+                    last_brief_ask[str(member) + "@" + str(guild.id)] = (
+                        rightnow() + 8
+                    )  # 8 seconds
+                    task2 = asyncio.create_task(
+                        ask_for_brief(), name=f"ask for brief {str(member)}@{guild.id}"
+                    )
+                    await task2
 
     @bot.tree.command()
     async def submit(interaction: discord.Interaction):
@@ -60,7 +194,7 @@ def run():
         data = r.json()
         status_code = r.status_code
         await ctx.send(f"status code: {status_code}\n{data}")
-    
+
     @bot.tree.context_menu(name="Pause Task!")
     async def pause_task(interaction: discord.Interaction, message: discord.Message):
         await interaction.response.send_message("this is not a task!")
