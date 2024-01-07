7b59a382b0df42ae89034a410279b8809e291ce9
Ali Kharrati <ali.kharrati@gmail.com>
2024-01-07 12:54:55 +0330

    CHANGE token expire to 5 mins

--- bot_auth.py
+++ bot_auth.py
@@ -10,7 +10,7 @@ def create_token(d: dict):
     load_dotenv()
     token = None
     now = datetime.datetime.now(tz=pytz.utc)
-    expires_at = now + datetime.timedelta(0, 30)  # 30 seconds
+    expires_at = now + datetime.timedelta(0, 300)  # 5 mins
     d["expires_at"] = expires_at.strftime("%Y-%m-%dT%H:%M:%S%z")
     d["is_genuine"] = getenv("PEPPER")
     token = jwt.encode(d, getenv("SALT"), algorithm="HS256")
