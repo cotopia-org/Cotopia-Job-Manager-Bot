da6279a07e9f26a3a5b5b25f4e387716fba3d3c4
Ali Kharrati <ali.kharrati@gmail.com>
2024-02-04 23:26:43 +0330

    MOVE create_tables() to file

--- timetracker/utils.py
+++ timetracker/utils.py
@@ -10,52 +10,6 @@ def rightnow():
     return epoch
 
 
-def create_tables():
-    conn = psycopg2.connect(
-        host="localhost",
-        dbname="postgres",
-        user="postgres",
-        password="Tp\ZS?gfLr|]'a",
-        port=5432,
-    )
-    cur = conn.cursor()
-    cur.execute(
-        """CREATE TABLE IF NOT EXISTS job_event(
-            id SERIAL NOT NULL PRIMARY KEY,
-            guild_id BIGINT NOT NULL,
-            discord_id BIGINT NOT NULL,
-            start_epoch BIGINT NOT NULL,
-            end_epoch BIGINT NULL,
-            duration INT NULL,
-            is_job BOOLEAN DEFAULT TRUE,
-            job_id INT NULL,
-            brief_id INT NULL,
-            title VARCHAR(255) NULL)
-            ;"""
-    )
-    conn.commit()
-    cur.close()
-    conn.close()
-
-    print("job_event created! @postgres")
-
-    conn = sqlite3.connect("jobs.db")
-    cursor = conn.cursor()
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS job_pendings(
-                                guild_id INT NOT NULL,
-                                discord_id INT NOT NULL,
-                                event_id INT NOT NULL,
-                                UNIQUE(guild_id, discord_id)
-                                ); """
-    )
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    print("job_pendings created! @jobs.db")
-
-
 def record_event(guild_id: int, discord_id: int, isjob: bool, id: int, title: str):
     conn = psycopg2.connect(
         host="localhost",
