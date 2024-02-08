6804019c2fc0d9a6556f8bf96d864412e4fce33f
Ali Kharrati <ali.kharrati@gmail.com>
2024-02-08 17:38:41 +0330

    ADD create postgres tables that's needed after ditching sqlite

--- timetracker/create_db.py
+++ timetracker/create_db.py
@@ -1,5 +1,3 @@
-import sqlite3
-
 import psycopg2
 
 
@@ -11,8 +9,8 @@ def create_tables():
         password="Tp\ZS?gfLr|]'a",
         port=5432,
     )
-    cur = conn.cursor()
-    cur.execute(
+    cursor = conn.cursor()
+    cursor.execute(
         """CREATE TABLE IF NOT EXISTS job_event(
             id SERIAL NOT NULL PRIMARY KEY,
             guild_id BIGINT NOT NULL,
@@ -26,14 +24,9 @@ def create_tables():
             title VARCHAR(255) NULL)
             ;"""
     )
-    conn.commit()
-    cur.close()
-    conn.close()
 
     print("job_event created! @postgres")
 
-    conn = sqlite3.connect("jobs.db")
-    cursor = conn.cursor()
     cursor.execute(
         """CREATE TABLE IF NOT EXISTS job_pendings(
                                 guild_id INT NOT NULL,
@@ -42,11 +35,39 @@ def create_tables():
                                 UNIQUE(guild_id, discord_id)
                                 ); """
     )
+
+    print("job_pendings created! @postgres")
+
+    cursor.execute(
+            """CREATE TABLE IF NOT EXISTS idles(
+                                guild_id INT NOT NULL,
+                                member_id INT NOT NULL); """
+    )
+
+    print("idles created! @postgres")
+
+    cursor.execute("""   CREATE TABLE IF NOT EXISTS status_txt(
+                                guild_id INT NOT NULL,
+                                channel_id INT NOT NULL,
+                                msg_id INT NOT NULL); """
+    )
+
+    print("status_txt created! @postgres")
+
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS job_posts(
+                   guild_id INT NOT NULL,
+                   channel_id INT NOT NULL,
+                   post_id INT NOT NULL,
+                   job_id INT NOT NULL
+                   );"""
+    )
+
+    print("job_posts created! @postgres")
+
     conn.commit()
     cursor.close()
     conn.close()
-
-    print("job_pendings created! @jobs.db")
 
 
 create_tables()
