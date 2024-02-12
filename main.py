b5369389ededdedb8de2aef7d6a0ed6da7095f93
Ali Kharrati <ali.kharrati@gmail.com>
2024-02-12 14:50:37 +0330

    CHANGE add a little space between emojies

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def run():
                             driver=str(message.guild.id),
                         )
                         em = discord.Embed(
-                            title="📣📣 Status Update",
+                            title="📣 📣 Status Update",
                             description=message.content,
                             color=discord.Color.blue(),
                         )
